812c6b838c0a6514884d1f07b1fab3aa8708e10e
smessie <smessie@smessie.com>
2021-11-21 21:46:22 +0100

    Add Session ID and auth middleware

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import logger from 'morgan';
 import {host, port} from './helpers.js';
 import RapidManager from './rapid/RapidManager.js';
 import routes from './routes/index.js';
+import uid from 'uid-safe';
 
 
 // Setup rapid river.
@@ -16,6 +17,28 @@ index.use(logger('dev'));
 index.use(express.json());
 index.use(express.urlencoded({extended: false}));
 index.use(cookieParser());
+
+// Session ID middleware.
+// Create new session ID and put it in a HTTP Only Cookie if no session ID exists yet.
+index.use(async (req, res, next) => {
+  let token = req.cookies.session;
+  if (!token) {
+    token = await uid(18);
+    res.cookie('session', token, {maxAge: 900000, httpOnly: true});
+  }
+  res.locals.sessionID = token;
+  next();
+});
+
+// Auth middleware.
+// Get the JWT token out of the authorization header if this header was included in the request.
+index.use((req, res, next) => {
+  let token = req.headers.Authorization;
+  if (token && token.includes('Bearer ')) {
+    res.locals.jwtToken = token.split('Bearer ')[1];
+  }
+  next();
+});
 
 // Define all different routers.
 routes(rapidManager).forEach(route => index[route.type](route.path, async (req, res) => {
