9b1569d4409ea5f874608860d1fb479ce83dd844
EmilOvcina <44057269+EmilOvcina@users.noreply.github.com>
2021-12-12 11:31:10 +0100

    Update crudDelete.js

--- src/routes/crudDelete.js
+++ src/routes/crudDelete.js
@@ -1,13 +1,11 @@
 export default function (rapidManager) {
     return {
       type: "delete",
-      path: "/files",
+      path: "/files/:id",
       auth: false,
       callback: async (req, res) => {
         await rapidManager.publishAndSubscribe("delete-file", "delete-file-response", res.locals.sessionID, {
-          filename: req.body.filename,
-          userId: res.locals.userId,
-          filetype: req.body.filetype
+          fileId: req.params.id
         }, resp => {
           res.send(resp);
         }, res.locals.userId);
