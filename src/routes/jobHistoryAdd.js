25bddf080e21613b8a8932ffb182fca451dc2779
Michael <clonex_kontakt@hotmail.com>
2021-12-12 15:16:01 +0100

    Updated jobHIstoryAdd data

--- src/routes/jobHistoryAdd.js
+++ src/routes/jobHistoryAdd.js
@@ -7,8 +7,7 @@ export default function (rapidManager) {
         const d = req.body;
         await rapidManager.publishAndSubscribe('add-job', 'add-job-response', res.locals.sessionID, {
           userID: res.locals.userId,
-          modelID: d.modelID,
-          dataID: d.dataID,
+          solvers: d.solvers,
         }, resp => {
           res.send({
             error: !(resp && !resp.error)
