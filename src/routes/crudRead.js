e28e9db5439101623492269f6b53fb44d36235ff
EmilOvcina <44057269+EmilOvcina@users.noreply.github.com>
2021-12-12 10:57:47 +0100

    Update crudRead.js

--- src/routes/crudRead.js
+++ src/routes/crudRead.js
@@ -1,11 +1,11 @@
 export default function (rapidManager) {
     return {
       type: "get",
-      path: "/files/read",
+      path: "/files/:filename",
       auth: false,
       callback: async (req, res) => {
         await rapidManager.publishAndSubscribe("read-file", "read-file-response", res.locals.sessionID, {
-          filename: req.body.filename,
+          filename: req.params.filename,
           userId: res.locals.userId,
           filetype: req.body.filetype
         }, resp => {
