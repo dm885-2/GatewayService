6a5d053e4fe7a8f7bd804dc41d27f5862a148f20
Michael <clonex_kontakt@hotmail.com>
2021-12-19 11:09:34 +0100

    Updated POST /jobs to include data/model IDs

--- src/routes/jobHistoryAdd.js
+++ src/routes/jobHistoryAdd.js
@@ -8,6 +8,8 @@ export default function (rapidManager) {
         const d = req.body;
         await rapidManager.publishAndSubscribe('add-job', 'add-job-response', res.locals.sessionID, {
           userID: res.locals.userId,
+          dataset: d.dataset,
+          model: d.model,
           solvers: d.solvers,
         }, resp => {
           res.send({
