bbfde1d5668dda842779d54a867a45ecef632878
Kaaasper <43987931+Kaaasper@users.noreply.github.com>
2022-02-14 04:20:04 +0100

    no cors

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import uid from 'uid-safe';
-import cors from 'cors';
+//import cors from 'cors';
 
 import {getTokenData, host, port} from './helpers.js';
 import RapidManager from './rapid/RapidManager.js';
